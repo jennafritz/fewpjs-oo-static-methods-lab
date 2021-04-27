4d1495f2a27575bde2e6ec380e3e9e0b1f1ae83c
jennafritz <jennafritz24@gmail.com>
2021-04-27 13:41:56 -0400

    commit tree 6f03d764624715aa46711792edcede8185a0de7d

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let capitalized = string[0].toUpperCase() + string.slice(1)
+    return capitalized
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
 }
